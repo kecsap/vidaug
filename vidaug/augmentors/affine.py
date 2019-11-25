43412605c0db43701b1cea144f876d8b8606a11d
kavinvin <kavinvin.vin@gmail.com>
2019-11-25 21:00:24 +0700

    Refactor the scale transformation

--- vidaug/augmentors/affine.py
+++ vidaug/augmentors/affine.py
@@ -18,6 +18,7 @@ import numpy as np
 import numbers
 import random
 import scipy
+from scipy import ndimage
 import PIL
 import cv2
 
@@ -49,7 +50,7 @@ class RandomRotate(object):
     def __call__(self, clip):
         angle = random.uniform(self.degrees[0], self.degrees[1])
         if isinstance(clip[0], np.ndarray):
-            rotated = [scipy.misc.imrotate(img, angle) for img in clip]
+            rotated = [ndimage.rotate(img, angle) for img in clip]
         elif isinstance(clip[0], PIL.Image.Image):
             rotated = [img.rotate(angle) for img in clip]
         else:
@@ -65,7 +66,7 @@ class RandomResize(object):
 
     Args:
         rate (float): Video is scaled uniformly between
-        [1 - rate, 1 + rate].
+        [1-rate, 1+rate].
 
         interp (string): Interpolation to use for re-sizing
         ('nearest', 'lanczos', 'bilinear', 'bicubic' or 'cubic').
@@ -77,35 +78,48 @@ class RandomResize(object):
         self.interpolation = interp
 
     def __call__(self, clip):
-        scaling_factor = random.uniform(1 - self.rate, 1 + self.rate)
+        scaling_factor = random.uniform(1-self.rate, 1+self.rate)
 
-        if isinstance(clip[0], np.ndarray):
-            im_h, im_w, im_c = clip[0].shape
-        elif isinstance(clip[0], PIL.Image.Image):
-            im_w, im_h = clip[0].size
+        is_PIL = isinstance(clip[0], PIL.Image.Image)
+        if is_PIL:
+            clip = [np.asarray(img) for img in clip]
 
-        new_w = int(im_w * scaling_factor)
-        new_h = int(im_h * scaling_factor)
-        new_size = (new_h, new_w)
-        if isinstance(clip[0], np.ndarray):
-            return [scipy.misc.imresize(img, size=(new_h, new_w),interp=self.interpolation) for img in clip]
-        elif isinstance(clip[0], PIL.Image.Image):
-            return [img.resize(size=(new_w, new_h), resample=self._get_PIL_interp(self.interpolation)) for img in clip]
+        im_h = clip[0].shape[0]
+        im_w = clip[0].shape[1]
+        new_w = int(im_w*scaling_factor)
+        new_h = int(im_h*scaling_factor)
+        data_final = []
+        for image in clip:
+            new_image = cv2.resize(image, (new_w, new_h), interpolation = self._get_cv2_interp(self.interpolation))
+            if scaling_factor >= 1.0:
+                top_x = (new_w-im_w) // 2
+                top_y = (new_h-im_h) // 2
+                cropped_image = new_image[top_y:top_y+im_h, top_x:top_x+im_w]
+                data_final.append(cropped_image)
+            else:
+                border_w1 = (im_w-new_w) // 2
+                border_w2 = im_w-new_w-border_w1
+                border_h1 = (im_h-new_h) // 2
+                border_h2 = im_h-new_h-border_h1
+                cropped_image = cv2.copyMakeBorder(new_image, border_h1, border_h2, border_w1, border_w2, cv2.BORDER_CONSTANT, 0)
+                data_final.append(cropped_image)
+
+        if is_PIL:
+            return [PIL.Image.fromarray(img) for img in data_final]
         else:
-            raise TypeError('Expected numpy.ndarray or PIL.Image' +
-                            'but got list of {0}'.format(type(clip[0])))
+            return data_final
 
-    def _get_PIL_interp(self, interp):
+    def _get_cv2_interp(self, interp):
         if interp == 'nearest':
-            return PIL.Image.NEAREST
+            return cv2.INTER_NEAREST
         elif interp == 'lanczos':
-            return PIL.Image.LANCZOS
+            return cv2.INTER_LANCZOS4
         elif interp == 'bilinear':
-            return PIL.Image.BILINEAR
+            return cv2.INTER_LINEAR
         elif interp == 'bicubic':
-            return PIL.Image.BICUBIC
+            return cv2.INTER_CUBIC
         elif interp == 'cubic':
-            return PIL.Image.CUBIC
+            return cv2.INTER_CUBIC
 
 
 class RandomTranslate(object):
